0e7076031102ec6e907e6f02678f39bf9dfea08c
OfeliaGarc <ofee.50.og@gmail.com>
2017-02-15 17:42:15 +0000

    un pequeño cambio

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 console.log("el documento está listo!!! :D")
+
+function(){
+  var Form = $('#progressive-validation');
+  Form.on('keyup',function(){
+    $('input:valid ~ input').attr('disabled',false);
+    $('input:invalid ~ input').attr('disabled',true);
+    alert("hola");
+  }
+
+
+
+// function suma_dos_numeros() {
+//   resultado = num1 + num2;
+//   alert("El resultado es: " + resultado);
+// }
+
+// var resultado;
+
+// var num1 = 20;
+// var num2 = 25;
+
+// suma_dos_numeros();
